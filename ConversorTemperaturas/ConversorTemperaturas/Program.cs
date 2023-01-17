7535f5241f0d914dae1d3e1c842102dee6026ac2
vilaJJ <vilajjlol@gmail.com>
2023-01-17 17:14:40 -0300

    Finalizada a seção de Operadores

---
+++ ConversorTemperaturas/ConversorTemperaturas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperaturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, fahrenheint, kelvin;
+
+            Console.WriteLine("### Conversor de Temperatura ###");
+
+            Console.Write("Insira a temperatura em Celsius: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------------------------------------------------");
+
+            // Fórmula de conversão de °C para °F: ((celsius * 9) / 5 ) + 32 = °F
+            fahrenheint = ((celsius * 9) / 5) + 32;
+
+            // Fórmula de conversão de °C para K: celsius + 273,15
+            kelvin = celsius + 273.15;
+
+            Console.WriteLine(celsius + "° C = " + fahrenheint + "° F");
+            Console.WriteLine(celsius + "° C = " + kelvin + " K");
+
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
