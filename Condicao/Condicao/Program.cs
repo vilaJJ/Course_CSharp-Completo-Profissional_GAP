6569198daa5437affa19d385855083517b98b6ed
vilaJJ <vilajjlol@gmail.com>
2023-01-18 10:11:59 -0300

    2/6 Estruturas condicionais

---
+++ Condicao/Condicao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 10;
+
+            if (valor == 10)
+            {
+                Console.WriteLine("Condição verdadeira.");
+            }
+            else
+            {
+                Console.WriteLine("Condição falsa.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
