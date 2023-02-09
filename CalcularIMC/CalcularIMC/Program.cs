37c201a6340cb123b1ce4bc8d407d6a4e46641ad
vilaJJ <vilajjlol@gmail.com>
2023-02-09 15:43:26 -0300

    Finalizada a seção de Condições

---
+++ CalcularIMC/CalcularIMC/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // peso / altura²
+
+            Console.Write("Informe o peso em kg: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe sua altura em metros: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double valor_imc = (peso / (altura * altura));
+
+            Console.Write("Seu IMC é: " + valor_imc + ". ");
+            if (valor_imc < 20)
+            {
+                Console.WriteLine("Você está abaixo do peso");
+            }
+            else if ((valor_imc >= 20) && (valor_imc <= 24))
+            {
+                Console.WriteLine("Seu peso esta normal.");
+            }
+            else if ((valor_imc > 24) && (valor_imc <= 29))
+            {
+                Console.WriteLine("Você está acima do peso.");
+            }
+            else if ((valor_imc > 29) && (valor_imc <= 34))
+            {
+                Console.WriteLine("Você está obeso.");
+            }
+            else
+            {
+                Console.Write("Você está muito obeso.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Condicao/Condicao/Program.cs
+++ Condicao/Condicao/Program.cs
@@ -10,18 +10,76 @@ namespace Condicao
     {
         static void Main(string[] args)
         {
-            int valor = 10;
-
-            if (valor == 10)
+            #region Condição encadeada
+            /*uint valor = 25;
+            if (valor < 10)
             {
-                Console.WriteLine("Condição verdadeira.");
+                Console.WriteLine("Condição alternativa 1.");
+            }
+            else if (valor >= 10 && valor < 20)
+            {
+                Console.WriteLine("Condição alternativa 2.");
+            }
+            else if (valor >= 20 && valor < 30)
+            {
+                Console.WriteLine("Condição alternativa 3.");
             }
             else
             {
-                Console.WriteLine("Condição falsa.");
+                Console.WriteLine("Condição alternativa 4");
             }
 
-            Console.ReadKey();
+            Console.ReadKey();*/
+            #endregion
+
+            #region Condições aninhadas
+            /*int num = 10;
+
+            if (num > 5)
+            {
+                Console.Write("O número é maior que 5");
+
+                if ((num % 2) == 0)
+                {
+                    Console.WriteLine(" e também é par.");
+                }
+                else
+                {
+                    Console.WriteLine(" e também é impar.");
+                }
+            }
+            else if (num == 5)
+            {
+                Console.Write("O número é igual a 5.");
+            }
+            else
+            {
+                Console.Write("O número é menor que 5");
+            }
+
+            Console.ReadKey();*/
+            #endregion
+
+            #region Operador ternário
+            /*int valor = 50;
+            string msg = "";
+
+            *//*if (valor > 50)
+            {
+                msg = "Maior que 50.";
+            }
+            else
+            {
+                msg = "Menor que 50.";
+            }*//*
+
+            // Sintaxe: condição ? true : false;
+
+            msg = valor > 50 ? "Maior que 50." : "Menor que 50.";
+
+            Console.WriteLine(msg);
+            Console.ReadKey();*/
+            #endregion
         }
     }
 }
