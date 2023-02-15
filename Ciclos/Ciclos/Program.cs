793da97a5dca8c3cfcddbb8b2a41691bcb7fdbe1
vilaJJ <vilajjlol@gmail.com>
2023-02-15 14:39:14 -0300

    Finalizada a seção de Repetição

---
+++ Ciclos/Ciclos/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region while
+            /*int valor = 1;
+            while (valor <= 10)
+            {
+                Console.WriteLine("Valor = " + valor);
+                valor++;
+            }
+
+            Console.Write("\nPrograma finalizado! \nPressione qualquer tecla para sair...");
+            Console.ReadKey();*/
+            #endregion
+
+            #region do-while
+            /*int valor = 20;
+            do
+            {
+                Console.WriteLine("Valor: " + valor);
+                valor++;
+            } while (valor <= 15);
+
+            Console.Write("\nPrograma finalizado!\nPressione qualquer tecla para sair...");
+            Console.ReadKey();*/
+            #endregion
+
+            #region for
+            /*for (int i = 1, x = 10; i <= 10 && x >= 1; i++, x--)
+            {
+                Console.WriteLine("\nValor de (i): " + i);
+                Console.WriteLine("Valor de (x): " + x);
+            }
+
+            Console.Write("\nPrograma finalizado!\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();*/
+            #endregion
+
+            #region foreach
+            string[] nomes =
+            {
+                "Juan Felipe Alves Flores",
+                "Maria Eduarda Batista de Sousa",
+                "Hiago de Oliveira Santana",
+                "Kevin Matheus Queiroz de Santana"
+            };
+
+            foreach (string item in nomes)
+            {
+                Console.WriteLine("Nome: " + item);
+            }
+
+            Console.Write("\nPrograma finalizado!\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 0 1 1 2 3 5 8 13 21 34 55 89 ...
+            int anterior1 = 0, anterior2 = 1, resultado = 0;
+
+            Inicio:
+            Console.Clear();
+            Console.Write("Quantos valores da Sequência de Fibonacci deseja gerar? ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSequência de Fibonnaci com " + quantidade + " valores:");
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                if(i < (quantidade - 1))
+                {
+                    Console.Write(anterior1 + ", ");
+                }
+                else
+                {
+                    Console.Write(anterior1 + ".");
+                }
+                
+
+                resultado = anterior1 + anterior2;
+                anterior1 = anterior2;
+                anterior2 = resultado;
+            }
+
+            TentarSaidaNovamente:
+            Console.Write("\n\nDeseja gerar outra sequência com outros valores? (s para sim, n para não): ");
+            char opcaoSaida = char.ToLower(char.Parse(Console.ReadLine()));
+
+            switch (opcaoSaida)
+            {
+                default:
+                    Console.WriteLine("Opção inválida! Tente novamente.");
+                    goto TentarSaidaNovamente;
+                case 's':
+                    goto Inicio;
+                case 'n':
+                    break;
+            }
+
+            Console.Write("\nPrograma finalizado!\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
