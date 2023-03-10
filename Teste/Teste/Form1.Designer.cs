7799eb3dfbdc41000efcfbde5be2aba3866d96a7
Juan Felipe Alves Flores <vilajjlol@gmail.com>
2023-03-10 17:57:57 -0300

    1/11 Seção 14. Windows Forms App de modo geral

---
+++ Teste/Teste/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botao
+            // 
+            this.botao.Location = new System.Drawing.Point(290, 177);
+            this.botao.Name = "botao";
+            this.botao.Size = new System.Drawing.Size(232, 104);
+            this.botao.TabIndex = 0;
+            this.botao.Text = "Executar";
+            this.botao.UseVisualStyleBackColor = true;
+            this.botao.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao;
+    }
+}
+
