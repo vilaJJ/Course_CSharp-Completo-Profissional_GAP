66939c241344f4bfb497dba9ec33e6276964cb5c
Juan Felipe Alves Flores <vilajjlol@gmail.com>
2023-05-15 11:56:04 -0300

    7/14 Seção 17. Coleções Avançadas

---
+++ Colecoes/Colecoes/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Colecoes
+{
+    partial class Colecoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.button_List = new System.Windows.Forms.Button();
+            this.button_limpar = new System.Windows.Forms.Button();
+            this.button_HashSet = new System.Windows.Forms.Button();
+            this.button_Dictionary = new System.Windows.Forms.Button();
+            this.button_SortedList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 25;
+            this.lista.Location = new System.Drawing.Point(0, 0);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(585, 561);
+            this.lista.TabIndex = 0;
+            // 
+            // button_List
+            // 
+            this.button_List.Location = new System.Drawing.Point(591, 12);
+            this.button_List.Name = "button_List";
+            this.button_List.Size = new System.Drawing.Size(181, 35);
+            this.button_List.TabIndex = 1;
+            this.button_List.Text = "List";
+            this.button_List.UseVisualStyleBackColor = true;
+            this.button_List.Click += new System.EventHandler(this.button_List_Click);
+            // 
+            // button_limpar
+            // 
+            this.button_limpar.Location = new System.Drawing.Point(690, 520);
+            this.button_limpar.Name = "button_limpar";
+            this.button_limpar.Size = new System.Drawing.Size(82, 29);
+            this.button_limpar.TabIndex = 2;
+            this.button_limpar.Text = "Limpar";
+            this.button_limpar.UseVisualStyleBackColor = true;
+            this.button_limpar.Click += new System.EventHandler(this.button_limpar_Click);
+            // 
+            // button_HashSet
+            // 
+            this.button_HashSet.Location = new System.Drawing.Point(591, 53);
+            this.button_HashSet.Name = "button_HashSet";
+            this.button_HashSet.Size = new System.Drawing.Size(181, 35);
+            this.button_HashSet.TabIndex = 3;
+            this.button_HashSet.Text = "HashSet";
+            this.button_HashSet.UseVisualStyleBackColor = true;
+            this.button_HashSet.Click += new System.EventHandler(this.button_HashSet_Click);
+            // 
+            // button_Dictionary
+            // 
+            this.button_Dictionary.Location = new System.Drawing.Point(591, 94);
+            this.button_Dictionary.Name = "button_Dictionary";
+            this.button_Dictionary.Size = new System.Drawing.Size(181, 35);
+            this.button_Dictionary.TabIndex = 4;
+            this.button_Dictionary.Text = "Dictionary";
+            this.button_Dictionary.UseVisualStyleBackColor = true;
+            this.button_Dictionary.Click += new System.EventHandler(this.button_Dictionary_Click);
+            // 
+            // button_SortedList
+            // 
+            this.button_SortedList.Location = new System.Drawing.Point(591, 135);
+            this.button_SortedList.Name = "button_SortedList";
+            this.button_SortedList.Size = new System.Drawing.Size(181, 34);
+            this.button_SortedList.TabIndex = 5;
+            this.button_SortedList.Text = "SortedList";
+            this.button_SortedList.UseVisualStyleBackColor = true;
+            this.button_SortedList.Click += new System.EventHandler(this.button_SortedList_Click);
+            // 
+            // Colecoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button_SortedList);
+            this.Controls.Add(this.button_Dictionary);
+            this.Controls.Add(this.button_HashSet);
+            this.Controls.Add(this.button_limpar);
+            this.Controls.Add(this.button_List);
+            this.Controls.Add(this.lista);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Colecoes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listas Genéricas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button button_List;
+        private System.Windows.Forms.Button button_limpar;
+        private System.Windows.Forms.Button button_HashSet;
+        private System.Windows.Forms.Button button_Dictionary;
+        private System.Windows.Forms.Button button_SortedList;
+    }
+}
+
---
+++ Colecoes/Colecoes/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Colecoes : Form
+    {
+        public Colecoes()
+        {
+            InitializeComponent();
+        }
+
+        private void button_List_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            // Exemplo de array
+            string[] nomes1 = new string[3];
+            nomes1[0] = "Juan";
+            nomes1[1] = "Duda";
+            nomes1[2] = "Yasmin";
+
+            List<string> nomes2 = new List<string>();
+
+            nomes2.AddRange(nomes1);
+            nomes2.Add("Beatriz");
+            nomes2.Add("Bárbara");
+
+            //lista.Items.Add(nomes2[0]);
+            //lista.Items.Add(nomes2[1]);
+
+            //lista.Items.AddRange(nomes1);
+
+            // MessageBox.Show(nomes2.Remove("Beatriz") ? "Beatriz foi removida" : "Beatriz não foi removida. Não existe na lista.");
+
+            // MessageBox.Show(nomes2.Contains("Bárbara") ? "Bárbara está na lista." : "Bárbara não está na lista.");
+
+            // nomes2.Sort(); // Organiza em ordem alfábetica
+
+            // MessageBox.Show($"Yasmin está no indice: {nomes2.IndexOf("Yasmin")}."); // Retorna -1 quando não existe na lista.
+
+            // nomes2.Insert(2, "João"); // Adiciona João no indice 2, e coloca os demais para trás.
+
+            // nomes2.RemoveAt(4); // Remove o item no indice 4.
+
+            // nomes2.Clear(); // Remove todos os elementos da lista
+
+            // nomes2.Add("Juan"); // É possivel adicionar itens do mesmo nome (valor).
+
+            foreach (string item in nomes2)
+            {
+                lista.Items.Add(item);
+            }
+
+            // MessageBox.Show($"A lista inserida possui {nomes2.Count} itens.");
+        }
+
+        private void button_HashSet_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            HashSet<string> veiculos = new HashSet<string>()  // Não comporta itens repetidos
+            {
+                "Carro", "Moto", "Avião", "Barco", "Bicicleta"
+            };
+            
+            /*
+            MessageBox.Show(veiculos.First());
+            MessageBox.Show(veiculos.Last());
+            */
+
+            //MessageBox.Show(veiculos.ElementAt(3)); // Retorna o item do indice 3
+                       
+            /*
+            if (!(veiculos.Add("Moto")))
+            {
+                MessageBox.Show("Não foi possivel adicionar. O item já existe na lista.");
+            }
+
+            MessageBox.Show($"O item 'Carro' {(veiculos.Contains("Carro") ? "existe" : "não existe")} na lista.");
+            */
+
+            /*
+            if (veiculos.Remove("Moto"))
+            {
+                MessageBox.Show("'Moto' removido.");
+            }
+            */
+
+            // int quantidadeItens = veiculos.Count;
+
+            // veiculos.Clear(); // Limpa toda a HashSet
+
+            foreach (string item in veiculos)
+            {
+                lista.Items.Add(item);
+            }
+        }
+
+        private void button_Dictionary_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            Dictionary<int, string> alunos = new Dictionary<int, string>()
+            {
+                { 1379, "Juan" },
+                { 5731, "Duda" },
+                { 4826, "Bárbara" }
+            };
+
+            /*
+            try
+            {
+                alunos.Add(1379, "João");
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(string.Format("Não foi possivel adicionar. Motivo: {0}", erro.Message));
+            }
+            */
+
+            foreach (KeyValuePair<int, string> item in alunos)
+            {
+                // lista.Items.Add(item); // Retorna chaves e valores, na mesma linha
+                // lista.Items.Add(item.Key); // Retorna apenas as chaves
+                // lista.Items.Add(item.Value); // Retorna apenas os valores
+
+                lista.Items.Add($"{item.Key}: {item.Value}");
+            }
+
+            // int quantidade_alunos = alunos.Count;
+            // MessageBox.Show($"Quantidade de alunos: {alunos.Count}.");
+
+            // alunos.Remove(5731); // Remove o item da chave 5731
+
+            // alunos.Clear(); // Irá limpar toda a coleção
+
+            // KeyValuePair<int, string> primeiro = alunos.First(); // Retorna o primeiro item da coleção
+            //MessageBox.Show($"Primeiro: {primeiro.Key} = {primeiro.Value}.");
+
+            // KeyValuePair<int, string> ultimo = alunos.Last();
+            // MessageBox.Show($"Último: {ultimo.Key} = {ultimo.Value}.");
+
+            // alunos.ContainsKey(1379); // Irá retornar true se existir na lista uma chave igual a 1379.
+            // alunos.ContainsValue("Juan"); // Irá retornar true se existir na lista um valor igual a "Juan".
+        }
+
+        private void button_SortedList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            SortedList<int, string> tabela_interclasse = new SortedList<int, string>()
+            {
+                { 2, "JIFTO Masc. (Reserva)" },
+                { 3, "Desconhecidos (o resto)" },
+                { 1, "Sin's (e o Victor)" }
+            };
+
+            tabela_interclasse.Add(4, "Torcida");
+            tabela_interclasse.Add(0, "Arbitragem");
+
+            tabela_interclasse.RemoveAt(4);
+            tabela_interclasse.Remove(0);
+
+            MessageBox.Show($"{tabela_interclasse.Count} times participaram dos Jogos Internos - Vôlei de Quadra.");
+
+            // tabela.ContainsKey(3); // Irá retornar true se existir na lista uma chave igual a 3
+            // tabela.ContainsValue("Aurora"); // Irá retornar true se existir na lista um valor igual a "Aurora".
+
+            foreach (KeyValuePair<int, string> item in tabela_interclasse) // tabela_interclasse.Reverse()
+            {
+                lista.Items.Add($"{item.Key}° lugar: {item.Value}.");
+            }
+        }
+
+        private void button_limpar_Click(object sender, EventArgs e)
+        {
+            if (!(lista.Items.Count == 0))
+            {
+                lista.Items.Clear();
+                MessageBox.Show("Lista esvaziada.");
+            }            
+        }
+    }
+}
