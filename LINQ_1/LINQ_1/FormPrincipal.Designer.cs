830789eef60eec1ef4cf7a149729fb68b850ec1e
Juan Felipe Alves Flores <vilajjlol@gmail.com>
2023-05-23 18:21:04 -0300

    Finalizada a Seção 24: LING - Language Integrated Query

---
+++ LINQ_1/LINQ_1/FormPrincipal.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LINQ_1
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.textBox_comando = new System.Windows.Forms.TextBox();
+            this.button_Executar = new System.Windows.Forms.Button();
+            this.button_Where = new System.Windows.Forms.Button();
+            this.button_OrderBy = new System.Windows.Forms.Button();
+            this.button_GroupBy = new System.Windows.Forms.Button();
+            this.button_Agregacao = new System.Windows.Forms.Button();
+            this.button_OpElementos = new System.Windows.Forms.Button();
+            this.button_LINQLambda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Monocraft", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.HorizontalScrollbar = true;
+            this.lista.ItemHeight = 28;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.ScrollAlwaysVisible = true;
+            this.lista.Size = new System.Drawing.Size(563, 508);
+            this.lista.TabIndex = 0;
+            // 
+            // textBox_comando
+            // 
+            this.textBox_comando.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_comando.Location = new System.Drawing.Point(581, 12);
+            this.textBox_comando.Name = "textBox_comando";
+            this.textBox_comando.Size = new System.Drawing.Size(191, 26);
+            this.textBox_comando.TabIndex = 1;
+            this.textBox_comando.TextChanged += new System.EventHandler(this.textBox_comando_TextChanged);
+            // 
+            // button_Executar
+            // 
+            this.button_Executar.Location = new System.Drawing.Point(581, 44);
+            this.button_Executar.Name = "button_Executar";
+            this.button_Executar.Size = new System.Drawing.Size(191, 44);
+            this.button_Executar.TabIndex = 2;
+            this.button_Executar.Text = "Executar";
+            this.button_Executar.UseVisualStyleBackColor = true;
+            this.button_Executar.Click += new System.EventHandler(this.button_Executar_Click);
+            // 
+            // button_Where
+            // 
+            this.button_Where.Location = new System.Drawing.Point(581, 94);
+            this.button_Where.Name = "button_Where";
+            this.button_Where.Size = new System.Drawing.Size(191, 44);
+            this.button_Where.TabIndex = 3;
+            this.button_Where.Text = "Where";
+            this.button_Where.UseVisualStyleBackColor = true;
+            this.button_Where.Click += new System.EventHandler(this.button_Where_Click);
+            // 
+            // button_OrderBy
+            // 
+            this.button_OrderBy.Location = new System.Drawing.Point(581, 144);
+            this.button_OrderBy.Name = "button_OrderBy";
+            this.button_OrderBy.Size = new System.Drawing.Size(191, 44);
+            this.button_OrderBy.TabIndex = 4;
+            this.button_OrderBy.Text = "Order by";
+            this.button_OrderBy.UseVisualStyleBackColor = true;
+            this.button_OrderBy.Click += new System.EventHandler(this.button_OrderBy_Click);
+            // 
+            // button_GroupBy
+            // 
+            this.button_GroupBy.Location = new System.Drawing.Point(581, 194);
+            this.button_GroupBy.Name = "button_GroupBy";
+            this.button_GroupBy.Size = new System.Drawing.Size(191, 44);
+            this.button_GroupBy.TabIndex = 5;
+            this.button_GroupBy.Text = "Group by";
+            this.button_GroupBy.UseVisualStyleBackColor = true;
+            this.button_GroupBy.Click += new System.EventHandler(this.button_GroupBy_Click);
+            // 
+            // button_Agregacao
+            // 
+            this.button_Agregacao.Location = new System.Drawing.Point(581, 244);
+            this.button_Agregacao.Name = "button_Agregacao";
+            this.button_Agregacao.Size = new System.Drawing.Size(191, 44);
+            this.button_Agregacao.TabIndex = 6;
+            this.button_Agregacao.Text = "Operadores de Agregação";
+            this.button_Agregacao.UseVisualStyleBackColor = true;
+            this.button_Agregacao.Click += new System.EventHandler(this.button_Agregacao_Click);
+            // 
+            // button_OpElementos
+            // 
+            this.button_OpElementos.Location = new System.Drawing.Point(581, 294);
+            this.button_OpElementos.Name = "button_OpElementos";
+            this.button_OpElementos.Size = new System.Drawing.Size(191, 44);
+            this.button_OpElementos.TabIndex = 7;
+            this.button_OpElementos.Text = "Operadores de Elemento";
+            this.button_OpElementos.UseVisualStyleBackColor = true;
+            this.button_OpElementos.Click += new System.EventHandler(this.button_OpElementos_Click);
+            // 
+            // button_LINQLambda
+            // 
+            this.button_LINQLambda.Location = new System.Drawing.Point(581, 344);
+            this.button_LINQLambda.Name = "button_LINQLambda";
+            this.button_LINQLambda.Size = new System.Drawing.Size(191, 44);
+            this.button_LINQLambda.TabIndex = 8;
+            this.button_LINQLambda.Text = "LINQ Lambda";
+            this.button_LINQLambda.UseVisualStyleBackColor = true;
+            this.button_LINQLambda.Click += new System.EventHandler(this.button_LINQLambda_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button_LINQLambda);
+            this.Controls.Add(this.button_OpElementos);
+            this.Controls.Add(this.button_Agregacao);
+            this.Controls.Add(this.button_GroupBy);
+            this.Controls.Add(this.button_OrderBy);
+            this.Controls.Add(this.button_Where);
+            this.Controls.Add(this.button_Executar);
+            this.Controls.Add(this.textBox_comando);
+            this.Controls.Add(this.lista);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "FormPrincipal";
+            this.Text = "LINQ - (Language Integrated Query)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.TextBox textBox_comando;
+        private System.Windows.Forms.Button button_Executar;
+        private System.Windows.Forms.Button button_Where;
+        private System.Windows.Forms.Button button_OrderBy;
+        private System.Windows.Forms.Button button_GroupBy;
+        private System.Windows.Forms.Button button_Agregacao;
+        private System.Windows.Forms.Button button_OpElementos;
+        private System.Windows.Forms.Button button_LINQLambda;
+    }
+}
+
---
+++ LINQ_1/LINQ_1/FormPrincipal.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_1
+{
+    public partial class FormPrincipal : Form
+    {
+        List<string> lista_nomes;
+        List<int> lista_numeros;
+        Dictionary<string, double> lista_produtos;
+        Dictionary<string, string> lista_cidades;
+
+        public FormPrincipal()
+        {
+            #region Lista de nomes
+            lista_nomes = new List<string>();
+            lista_nomes.Add("Juan Felipe Alves Flores");
+            lista_nomes.Add("Maria Eduarda Batista de Sousa");
+            lista_nomes.Add("Bárbara Ohana dos Santos");
+            lista_nomes.Add("Yasmin Garcia Borges");
+            lista_nomes.Add("João Arthur Borges Rodrigues");
+            lista_nomes.Add("Jefferson Ribeiro Vasconcelos Carvalho");
+            #endregion
+
+            #region Lista de números
+            lista_numeros = new List<int>();            
+            lista_numeros.Add(7);
+            lista_numeros.Add(10);
+            lista_numeros.Add(21);
+            lista_numeros.Add(5);
+            lista_numeros.Add(1);
+            lista_numeros.Add(13);
+            #endregion
+
+            #region Lista de produtos
+            lista_produtos = new Dictionary<string, double>();
+            lista_produtos.Add("Apple Iphone 7s Plus", 1899.99);
+            lista_produtos.Add("Cabo USB-C 3.1", 79.99);
+            lista_produtos.Add("Monitor Dell 24' H2419U", 1399.99);
+            lista_produtos.Add("Xiaomi Poco X3 Pro", 2189.99);
+            lista_produtos.Add("Mac Mini Mid 11'", 1779.99);
+            lista_produtos.Add("Logitech C270 Pro", 249.99);
+            #endregion
+
+            #region Lista de cidades
+            lista_cidades = new Dictionary<string, string>();
+            lista_cidades.Add("Montreal", "Canada");
+            lista_cidades.Add("Araguaina", "Brazil");
+            lista_cidades.Add("Seoul", "South Korea");
+            lista_cidades.Add("Rio de Janeiro", "Brazil");
+            lista_cidades.Add("Busan", "South Korea");
+            lista_cidades.Add("Vancouver", "Canada");
+            #endregion
+
+            InitializeComponent();
+        }
+
+        private void button_Executar_Click()
+        {
+            button_Executar_Click(null, null);
+        }
+        private void button_Executar_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            // Obter a fonte dos dados
+            // Criar a consulta
+            // Executar a consulta
+
+            /*
+            IEnumerable<int> res1 = 
+                from num in lista_numeros
+                where num % 2 == 0
+                select num;
+
+            foreach (int item in res1)
+            {
+                lista.Items.Add(item);
+            }
+            */
+
+            IEnumerable<string> res2 =
+                from nome in lista_nomes
+                where nome.StartsWith(textBox_comando.Text)
+                select nome;
+            lista.Items.AddRange(res2.ToArray());
+        }
+
+        private void textBox_comando_TextChanged(object sender, EventArgs e)
+        {
+            // button_Executar_Click();
+        }
+
+        private void button_Where_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            // Operador de Filtragem, a cláusula where
+
+            IEnumerable<string> res1 =
+                from nome in lista_nomes
+                where nome.ToLower().Contains(textBox_comando.Text.ToLower())
+                select nome;
+            lista.Items.AddRange(res1.ToArray());
+        }
+
+        private void button_OrderBy_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            // Operador de ordenação
+
+            /*
+            IEnumerable<int> res1 =
+                from num in lista_numeros
+                orderby num ascending // descending
+                where num > 0
+                select num;
+
+            foreach (int item in res1)
+            {
+                lista.Items.Add(item);
+            }
+            */
+
+            /*
+            IEnumerable<string> res2 =
+                from nome in lista_nomes
+                orderby nome ascending // descending
+                select nome;
+            lista.Items.AddRange(res2.ToArray());
+            */
+
+            IEnumerable<KeyValuePair<string, double>> res3 =
+                from produto in lista_produtos
+                orderby produto.Key ascending // descending
+                select produto;
+            foreach (KeyValuePair<string, double> item in res3)
+            {
+                lista.Items.Add($"{item.Key} (R$ {item.Value.ToString("000,000.00").TrimStart('0', '.')})");
+            }
+        }
+
+        private void button_GroupBy_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            textBox_comando.Clear();
+            // Operadores de agrupamento
+
+            IEnumerable<IGrouping<string, KeyValuePair<string, string>>> res1 =
+                from cidade in lista_cidades
+                group cidade by cidade.Value;
+            foreach (IGrouping<string, KeyValuePair<string, string>> grupo in res1)
+            {
+                lista.Items.Add(grupo.Key);
+                foreach (var estado in grupo)
+                {
+                    lista.Items.Add($"\t-> {estado.Key}");
+                }
+            }
+        }
+
+        private void button_Agregacao_Click(object sender, EventArgs e)
+        {
+            string txtbox = textBox_comando.Text;
+            lista.Items.Clear();
+            textBox_comando.Clear();
+
+            // Operadores de Agregação
+            
+            // Count
+            int cont1 = lista_nomes.Count();
+            int cont2 = (from nome in lista_nomes where nome.StartsWith(txtbox) select nome).Count();
+
+            lista.Items.Add($"{cont1} nomes.");
+            lista.Items.Add($"{cont2} nomes começados com '{txtbox}'.");
+
+            // Average
+            double media1 = lista_numeros.Average();
+            lista.Items.Add($"Média em 'lista_numeros': {media1}");
+
+            double media2 = (from num in lista_numeros where num < 10 select num).Average();
+            lista.Items.Add($"Média em 'lista_numeros' < 10: {media2}");
+
+            // Sum
+            int soma1 = lista_numeros.Sum();
+            lista.Items.Add($"Soma em 'lista_numeros': {soma1}.");
+
+            int soma2 = (from num in lista_numeros where num < 10 select num).Sum();
+            lista.Items.Add($"Soma em 'lista_numeros' < 10: {soma2}.");
+
+            // Max
+            int max1 = lista_numeros.Max();
+            lista.Items.Add($"Maior número: {max1}.");
+
+            int max2 = (from num in lista_numeros where num % 2 == 0 select num).Max();
+            lista.Items.Add($"Maior número par: {max2}.");
+
+            // Min
+            int min1 = lista_numeros.Min();
+            lista.Items.Add($"Menor número: {min1}.");
+
+            int min2 = (from num in lista_numeros where num % 2 == 0 select num).Min();
+            lista.Items.Add($"Menor número par: {min2}.");
+
+            // LongCount
+            long contagem1 = lista_numeros.LongCount();
+            long contagem2 = (from num in lista_numeros where num % 2 == 0 select num).LongCount();
+
+            // Agregate
+            string maiorNome1 = lista_nomes.Aggregate(lista_nomes[0], (maior, proximo) =>
+            {
+                return (maior.Length > proximo.Length)
+                ? maior
+                : proximo;
+            });
+            string maiorNome2 = (from nome in lista_nomes select nome).Aggregate(lista_nomes[0], (maior, proximo) =>
+            {
+                return (maior.Length > proximo.Length)
+                ? maior
+                : proximo;
+            });
+
+            lista.Items.Add($"Maior nome: '{maiorNome2}'.");
+        }
+
+        private void button_OpElementos_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            // Operados de Elemento
+
+            /* Se não tiver elementos na lista, irá retornar erro se usar o .First();
+             * Usar o .FirstOrDefault() para contornar
+            int primeiro1 = lista_numeros.First();
+            int primeiro2 = (from num in lista_numeros where num % 2 == 0 select num).First();
+            */ 
+            int primeiro1 = lista_numeros.FirstOrDefault();
+            int primeiro2 = (from num in lista_numeros where num % 2 == 0 select num).FirstOrDefault();
+
+            lista.Items.Add($"Primeiro 'lista_numeros': {primeiro1}.");
+            lista.Items.Add($"Primeiro par 'lista_numeros': {primeiro2}.");
+
+            /* Se não tiver elementos na lista, irá retornar erro se usar o .ElementAt(x);
+             * Usar o .ElementAtOrDefault(x) para contornar
+            int elemento1 = lista_numeros.ElementAt(3);
+            int elemento2 = (from num in lista_numeros where num % 2 == 0 select num).ElementAt(3);
+            */
+            int elemento1 = lista_numeros.ElementAtOrDefault(3);
+            int elemento2 = (from num in lista_numeros where num % 2 == 0 select num).ElementAtOrDefault(3);
+
+            lista.Items.Add($"Elemento id. 3 'lista_numeros': {elemento1}.");
+            lista.Items.Add($"Último id. 3 entre pares 'lista_numeros': {elemento2}.");
+
+            /* Se não tiver elementos na lista, irá retornar erro se usar o .Last();
+             * Usar o .LastOrDefault() para contornar
+            int ultimo1 = lista_numeros.Last();
+            int ultimo2 = (from num in lista_numeros where num % 2 == 0 select num).Last();
+            */
+            int ultimo1 = lista_numeros.LastOrDefault();
+            int ultimo2 = (from num in lista_numeros where num % 2 == 0 select num).LastOrDefault();
+
+            lista.Items.Add($"Último 'lista_numeros': {ultimo1}.");
+            lista.Items.Add($"Último par 'lista_numeros': {ultimo2}.");
+        }
+
+        private void button_LINQLambda_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            // LINQ com Métodos Lambda
+            /*
+            var cons1 = (from nome in lista_nomes select nome);     // Comum
+            var cons2 = lista_nomes.Select(nome => nome);
+            lista.Items.AddRange(cons2.ToArray());
+            */
+
+            /*
+            var cons3 = (from nome in lista_nomes where nome.StartsWith(textBox_comando.Text) select nome);
+            var cons4 = lista_nomes.Where(nome => nome.StartsWith(textBox_comando.Text));
+            lista.Items.AddRange(cons4.ToArray());
+            */
+
+            /*
+            var cons5 = (from nome in lista_nomes orderby nome ascending select nome);
+            var cons6 = lista_nomes.OrderBy(nome => nome);
+            var cons7 = lista_nomes.OrderByDescending(nome => nome);
+            lista.Items.AddRange(cons7.ToArray());
+            */
+
+            var cons8 = (from cidade in lista_cidades group cidade by cidade.Value);
+            var cons9 = lista_cidades.GroupBy(estado => estado.Value);
+
+            foreach (var grupo in cons9)
+            {
+                lista.Items.Add(grupo.Key);
+                foreach (var estado in grupo)
+                {
+                    lista.Items.Add($"\t{estado.Key}");
+                }
+            }
+        }
+    }
+}
