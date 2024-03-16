896ebc356fab9b492c5c43ca103d205e38ef21bb
Juan Felipe Alves Flores <vilajjlol@gmail.com>
2024-03-16 02:28:44 -0300

    Finalizada a Seção 26: Trabalhar com impressão

--- Imprimir/Imprimir/FormPrincipal.Designer.cs
+++ Imprimir/Imprimir/FormPrincipal.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
             this.PrintDocument_Impressao = new System.Drawing.Printing.PrintDocument();
             this.Button_Imprimir = new System.Windows.Forms.Button();
+            this.PrintPreviewControl_Documento = new System.Windows.Forms.PrintPreviewControl();
+            this.Button_Visualizar = new System.Windows.Forms.Button();
+            this.Button_Anterior = new System.Windows.Forms.Button();
+            this.Button_Proximo = new System.Windows.Forms.Button();
+            this.Button_VisualizarPadrao = new System.Windows.Forms.Button();
+            this.PrintPreviewDialog_Impressao = new System.Windows.Forms.PrintPreviewDialog();
+            this.PrintDialog_ConfiguracaoImpressao = new System.Windows.Forms.PrintDialog();
             this.SuspendLayout();
             // 
             // PrintDocument_Impressao
@@ -42,16 +50,82 @@
             this.Button_Imprimir.Location = new System.Drawing.Point(608, 498);
             this.Button_Imprimir.Name = "Button_Imprimir";
             this.Button_Imprimir.Size = new System.Drawing.Size(164, 51);
-            this.Button_Imprimir.TabIndex = 0;
+            this.Button_Imprimir.TabIndex = 4;
             this.Button_Imprimir.Text = "&Imprimir";
             this.Button_Imprimir.UseVisualStyleBackColor = true;
             this.Button_Imprimir.Click += new System.EventHandler(this.Button_Imprimir_Click);
+            // 
+            // PrintPreviewControl_Documento
+            // 
+            this.PrintPreviewControl_Documento.Location = new System.Drawing.Point(12, 12);
+            this.PrintPreviewControl_Documento.Name = "PrintPreviewControl_Documento";
+            this.PrintPreviewControl_Documento.Size = new System.Drawing.Size(574, 537);
+            this.PrintPreviewControl_Documento.TabIndex = 0;
+            // 
+            // Button_Visualizar
+            // 
+            this.Button_Visualizar.Location = new System.Drawing.Point(608, 12);
+            this.Button_Visualizar.Name = "Button_Visualizar";
+            this.Button_Visualizar.Size = new System.Drawing.Size(164, 51);
+            this.Button_Visualizar.TabIndex = 1;
+            this.Button_Visualizar.Text = "&Visualizar";
+            this.Button_Visualizar.UseVisualStyleBackColor = true;
+            this.Button_Visualizar.Click += new System.EventHandler(this.Button_Visualizar_Click);
+            // 
+            // Button_Anterior
+            // 
+            this.Button_Anterior.Location = new System.Drawing.Point(608, 69);
+            this.Button_Anterior.Name = "Button_Anterior";
+            this.Button_Anterior.Size = new System.Drawing.Size(80, 51);
+            this.Button_Anterior.TabIndex = 2;
+            this.Button_Anterior.Text = "&Anterior";
+            this.Button_Anterior.UseVisualStyleBackColor = true;
+            this.Button_Anterior.Click += new System.EventHandler(this.Button_Anterior_Click);
+            // 
+            // Button_Proximo
+            // 
+            this.Button_Proximo.Location = new System.Drawing.Point(694, 69);
+            this.Button_Proximo.Name = "Button_Proximo";
+            this.Button_Proximo.Size = new System.Drawing.Size(80, 51);
+            this.Button_Proximo.TabIndex = 3;
+            this.Button_Proximo.Text = "&Próximo";
+            this.Button_Proximo.UseVisualStyleBackColor = true;
+            this.Button_Proximo.Click += new System.EventHandler(this.Button_Proximo_Click);
+            // 
+            // Button_VisualizarPadrao
+            // 
+            this.Button_VisualizarPadrao.Location = new System.Drawing.Point(608, 441);
+            this.Button_VisualizarPadrao.Name = "Button_VisualizarPadrao";
+            this.Button_VisualizarPadrao.Size = new System.Drawing.Size(164, 51);
+            this.Button_VisualizarPadrao.TabIndex = 5;
+            this.Button_VisualizarPadrao.Text = "Vis&ualizador Padrão";
+            this.Button_VisualizarPadrao.UseVisualStyleBackColor = true;
+            this.Button_VisualizarPadrao.Click += new System.EventHandler(this.Button_VisualizarPadrao_Click);
+            // 
+            // PrintPreviewDialog_Impressao
+            // 
+            this.PrintPreviewDialog_Impressao.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.PrintPreviewDialog_Impressao.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.PrintPreviewDialog_Impressao.ClientSize = new System.Drawing.Size(400, 300);
+            this.PrintPreviewDialog_Impressao.Enabled = true;
+            this.PrintPreviewDialog_Impressao.Icon = ((System.Drawing.Icon)(resources.GetObject("PrintPreviewDialog_Impressao.Icon")));
+            this.PrintPreviewDialog_Impressao.Name = "PrintPreviewDialog_Impressao";
+            this.PrintPreviewDialog_Impressao.Visible = false;
+            // 
+            // PrintDialog_ConfiguracaoImpressao
+            // 
+            this.PrintDialog_ConfiguracaoImpressao.UseEXDialog = true;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Button_VisualizarPadrao);
+            this.Controls.Add(this.Button_Proximo);
+            this.Controls.Add(this.Button_Anterior);
+            this.Controls.Add(this.Button_Visualizar);
+            this.Controls.Add(this.PrintPreviewControl_Documento);
             this.Controls.Add(this.Button_Imprimir);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -70,6 +144,13 @@
 
         internal System.Drawing.Printing.PrintDocument PrintDocument_Impressao;
         internal System.Windows.Forms.Button Button_Imprimir;
+        internal System.Windows.Forms.PrintPreviewControl PrintPreviewControl_Documento;
+        internal System.Windows.Forms.Button Button_Visualizar;
+        internal System.Windows.Forms.Button Button_Anterior;
+        internal System.Windows.Forms.Button Button_Proximo;
+        internal System.Windows.Forms.Button Button_VisualizarPadrao;
+        internal System.Windows.Forms.PrintPreviewDialog PrintPreviewDialog_Impressao;
+        internal System.Windows.Forms.PrintDialog PrintDialog_ConfiguracaoImpressao;
     }
 }
 
--- Imprimir/Imprimir/FormPrincipal.cs
+++ Imprimir/Imprimir/FormPrincipal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Printing;
 using System.Windows.Forms;
@@ -7,20 +8,44 @@ namespace Imprimir
 {
     public partial class FormPrincipal : Form
     {
+        private int pontoX, pontoY, largura, altura, numLinhas, paginaAtual, totalPaginas;
+
         public FormPrincipal()
         {
             InitializeComponent();
+
+            paginaAtual =
+            totalPaginas = 
+            0;
+        }
+
+        private void Button_VisualizarPadrao_Click(object sender, EventArgs e)
+        {
+            DefinirTamanho();
+            PrintPreviewDialog_Impressao.Document = PrintDocument_Impressao;
+            PrintPreviewDialog_Impressao.ShowDialog();
         }
 
         private void Button_Imprimir_Click(object sender, EventArgs e)
         {
-            PrintDocument_Impressao.Print();
+            DefinirTamanho();
+
+            PrintDialog_ConfiguracaoImpressao.Document = PrintDocument_Impressao;
+
+            if (PrintDialog_ConfiguracaoImpressao.ShowDialog() == DialogResult.OK)
+            {
+                DefinirTamanho();
+
+                PrintDocument_Impressao.PrinterSettings = PrintDialog_ConfiguracaoImpressao.PrinterSettings;
+                PrintDocument_Impressao.Print();
+            }            
         }
 
         private void PrintDocument_Impressao_PrintPage(object sender, PrintPageEventArgs e)
         {
             #region Parte 1
-
+            /*
+             
             var dados = "Trabalhando com impressão em C# (CS | CSharp).";
             var fonte = new Font("Arial", 16, FontStyle.Regular, GraphicsUnit.Point);
             var pincel = new SolidBrush(Color.Black);
@@ -46,7 +71,170 @@ namespace Imprimir
                 new Rectangle(100, 300, 100, 150) 
                 );
 
+            */
             #endregion
+
+            #region Parte 2
+            /*
+            var areaImpressao = PrintDocument_Impressao.DefaultPageSettings.Bounds;
+
+            var pontoX = areaImpressao.X;
+            var pontoY = areaImpressao.Y;
+            var largura = areaImpressao.Width;
+            var altura = areaImpressao.Height;
+
+            var textoPagina = "Título da Página";
+            var letraTitulo = new Font("Verdana", 24, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            var pincelTitulo = new SolidBrush(Color.Purple);
+            var canetaTitulo = new Pen(Color.Blue, 2);
+            var areaTextoTitulo = new Rectangle(pontoX + 50, pontoY + 100, largura - 100, 100);
+            var formatoTitulo = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            e.Graphics.DrawString(textoPagina, letraTitulo, pincelTitulo, areaTextoTitulo, formatoTitulo);
+            e.Graphics.DrawRectangle(canetaTitulo, areaTextoTitulo);
+
+            var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,";
+            var letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);
+            var pincel = new SolidBrush(Color.Black);
+            // var caneta = new Pen(Color.Red, 2);
+            var areaTexto = new Rectangle(pontoX + 50, pontoY + 200, largura - 100, altura - 400);
+
+            e.Graphics.DrawString(texto, letra, pincel, areaTexto);
+            // e.Graphics.DrawRectangle(caneta, areaTexto);
+            */
+            #endregion
+
+            #region Parte 3
+
+            var listaTexto = new List<string>
+            {
+                "01. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "02. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "03. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "04. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "05. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "06. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "07. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "08. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "09. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "10. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "11. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "12. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "13. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "14. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "15. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "16. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "17. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "18. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "19. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "20. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "21. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "22. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "23. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "24. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "25. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "26. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "27. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "28. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "29. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "30. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "31. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "32. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "33. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "34. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "35. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "36. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "37. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "38. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "39. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "40. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "41. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "42. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "43. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "44. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "45. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "46. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "47. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "48. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "49. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "50. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            };
+
+            var tituloPagina = "Título da Página";
+            var letraTitulo = new Font("Verdana", 24, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+            var pincelTitulo = new SolidBrush(Color.Purple);
+            var areaTextoTitulo = new Rectangle(pontoX, pontoY, largura - 100, 100);
+            var formatoTitulo = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            var letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);
+            var pincel = new SolidBrush(Color.Black);
+
+            while (numLinhas < listaTexto.Count)
+            {
+                if (numLinhas is 0)
+                {
+                    e.Graphics.DrawString(tituloPagina, letraTitulo, pincelTitulo, areaTextoTitulo, formatoTitulo);
+                    pontoY += 150;
+                }
+
+                var areaTexto = new Point(pontoX, pontoY);
+
+                e.Graphics.DrawString(listaTexto[numLinhas], letra, pincel, areaTexto);
+                pontoY += 30;
+                numLinhas++;
+
+                if (pontoY >= altura - 50)
+                {
+                    // Mudar de página
+                    pontoY = 50;
+                    e.HasMorePages = true;
+                    totalPaginas++;
+                    break;
+                }
+            }
+
+            #endregion
+        }
+
+        private void Button_Anterior_Click(object sender, EventArgs e)
+        {
+            if (paginaAtual > 0)
+            {
+                PrintPreviewControl_Documento.StartPage = --paginaAtual;
+            }
+        }
+
+        private void Button_Proximo_Click(object sender, EventArgs e)
+        {
+            if (paginaAtual < totalPaginas)
+            {
+                PrintPreviewControl_Documento.StartPage = ++paginaAtual;
+            }
+        }
+
+        private void Button_Visualizar_Click(object sender, EventArgs e)
+        {
+            DefinirTamanho();
+            PrintPreviewControl_Documento.Document = PrintDocument_Impressao;
+        }
+
+        private void DefinirTamanho()
+        {
+            var areaImpressao = PrintDocument_Impressao.DefaultPageSettings.Bounds;
+
+            pontoX = 50;
+            pontoY = 50;
+            largura = areaImpressao.Width;
+            altura = areaImpressao.Height;
+            numLinhas = 0;
         }
     }
 }
